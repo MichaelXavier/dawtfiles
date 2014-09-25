003c86d0149ed4802d65b567c78090c3e4425dc9
Michael Xavier <michael@michaelxavier.net>
2014-09-25 11:23:39 -0700

    fix flipped screen switching controls

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.Dmenu
+import qualified XMonad.StackSet as W
 
 import qualified Data.Map as M
 
@@ -10,4 +11,11 @@ main = xmonad gnomeConfig
        }
 
 overrideKeys (XConfig { modMask = modm}) = M.fromList $
-  [ ((modm, xK_p), spawn "dmenu_run")]
+    [ ((modm, xK_p), spawn "dmenu_run") ] ++ multiScreenMapping
+  where
+    -- copied from defaultConfig, but reversed because xinerama
+    -- numbers my screens backwards for some reason
+    multiScreenMapping =
+      [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+          | (key, sc) <- zip [xK_w, xK_e] [1, 0]
+          , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
