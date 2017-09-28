0ad7f686bbdc689af0d9d931574238d2a84188c5
Michael Xavier <michael@michaelxavier.net>
2017-09-28 09:25:34 -0700

    support mate and gnome xmonad confs

---
+++ .xmonad/xmonad-gnome.hs
@@ -0,0 +1,31 @@
+import           XMonad
+import           XMonad.Actions.NoBorders
+import           XMonad.Config.Gnome
+import qualified XMonad.StackSet          as W
+import           XMonad.Util.Dmenu
+import           XMonad.Util.SpawnOnce
+
+import qualified Data.Map                 as M
+
+main = xmonad gnomeConfig
+       { modMask = mod4Mask
+       , keys = overrideKeys <+> keys gnomeConfig
+       , startupHook = (startupHook gnomeConfig) >> myStartupHook
+       }
+
+myStartupHook = do
+  spawnOnce "nm-applet"
+
+overrideKeys (XConfig { modMask = modm}) = M.fromList $
+    [ ((modm, xK_p), spawn "$SHELL -c dmenu_run")
+    , ((modm .|. controlMask, xK_w), spawn "xcalib -invert -alter -s 0")
+    , ((modm .|. controlMask, xK_e), spawn "xcalib -invert -alter -s 1")
+    , ((modm, xK_g), withFocused toggleBorder)
+    ] ++ multiScreenMapping
+  where
+    -- copied from defaultConfig, but reversed because xinerama
+    -- numbers my screens backwards for some reason
+    multiScreenMapping =
+      [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+          | (key, sc) <- zip [xK_w, xK_e] [0, 1]
+          , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
